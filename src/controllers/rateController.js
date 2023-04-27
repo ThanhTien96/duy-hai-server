4250272f7d18d80575a00dc556ea6eda6ad271d8
Thanh Tien <dso791>
2023-04-27 17:57:47 +0700

    update

--- src/controllers/rateController.js
+++ src/controllers/rateController.js
@@ -80,78 +80,37 @@ const getRateWithProduct = async (req, res) => {
     };
 };
 
-const rateProduct = async (req, res) => {
-    try {
-      const { maDanhGia, maSanPham } = req.query;
-  
-      const product = await prisma.products.findUnique({
-        where: { maSanPham },
-      });
-  
-      if (!product) {
-        return res.status(404).json({ message: message.NOT_FOUND });
-      }
-  
-      const rate = await prisma.rates.findUnique({
-        where: { maDanhGia },
-        include: { sanPham: true },
-      });
-  
-      if (!rate) {
-        return res.status(404).json({ message: message.NOT_FOUND });
-      }
-  
-      const existingRateProduct = await prisma.rates_products.findUnique({
-        where: {
-          maDanhGia_maSanPham: {
-            maDanhGia: rate.maDanhGia,
-            maSanPham: product.maSanPham,
-          },
-        },
-      });
-  
-      if (!existingRateProduct) {
-        await prisma.rates_products.create({
-          data: {
-            maDanhGia: rate.maDanhGia,
-            maSanPham: product.maSanPham,
-          },
-        });
-      } else {
-        await prisma.rates_products.update({
-          where: {
-            maDanhGia_maSanPham: {
-              maDanhGia: rate.maDanhGia,
-              maSanPham: product.maSanPham,
-            },
-          },
-          data: {
-            maDanhGia: rate.maDanhGia,
-            maSanPham: product.maSanPham,
-          },
-        });
-      }
-  
-      res.status(200).json({ message: message.UPDATE });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: message.INTERNAL_SERVER_ERROR });
-    }
-  };
-
 const createRate = async (req, res) => {
     try {
 
-        const { soSao } = req.body;
+        const { soSao, maSanPham } = req.body;
 
         if(soSao <= 0 || soSao > 5) {
             return res.status(400).json({message: 'Số sao chỉ nhận từ 1 đến 5!'});
         };
 
+        const findProduct = await prisma.products.findFirst({
+            where: {
+                maSanPham: String(maSanPham)
+            }
+        });
+
+        if (!findProduct) {
+            return res.status(404).json({message: message.NOT_FOUND});
+        };
+
+        if(soSao > 5) {
+            return res.status(404).json({message: "Số sao không lớn hơn 5 !"})
+        }
 
         const data = await prisma.rates.create({
             data: {
                 soSao: soSao * 1,
+                sanPham: {
+                    create: {
+                        maSanPham: String(maSanPham)
+                    }
+                }
             },
             include: {
                 sanPham: {
@@ -195,7 +154,9 @@ const deleteRate = async (req, res) => {
         await prisma.rates_products.deleteMany({
             where: {
                 danhGia: {
-                    maDanhGia: String(maDanhGia)
+                    rates: {
+                        maDanhGia: String(maDanhGia)
+                    }
                 }
             }
         });
@@ -217,7 +178,6 @@ module.exports = {
     getAllRate,
     getDetailRate,
     getRateWithProduct,
-    rateProduct,
     createRate,
     deleteRate
 }
